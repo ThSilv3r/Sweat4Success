543a0e3c27a0b99f88d5a3c422ce6c617fd66a01
Jonas <lppoweryoshi@gmail.com>
2020-12-01 07:12:20 +0100

    Test  ertellen versuch

--- app/src/main/java/com/example/sweat4success/database/AppDatabase.kt
+++ app/src/main/java/com/example/sweat4success/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [UserDb::class], version = 1, exportSchema = false)
+@Database(entities = [UserDb::class, WorkoutDb::class, TagDb::class, ExerciseDb::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
