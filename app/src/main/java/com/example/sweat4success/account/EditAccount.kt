a272eadcdcd8f27a6a7958c90af476388dffb648
nilswendland <65414607+nilswendland@users.noreply.github.com>
2020-12-01 09:06:36 +0100

    UC Login test scenarios

---
+++ app/src/main/java/com/example/sweat4success/account/EditAccount.kt
@@ -0,0 +1,14 @@
+package com.example.sweat4success.account
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sweat4success.R
+
+
+class EditAccount: AppCompatActivity() {
+override fun onCreate(savedInstanceState: Bundle?){
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.editaccount)
+
+}
+}
