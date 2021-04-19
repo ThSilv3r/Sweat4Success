20915a586e9d0e806e60572df48bb67f6ccc8b0c
Marina Vollmer <mvollmer@allegion.com>
2021-04-19 18:29:37 +0200

    create workout categories layout

--- app/src/main/java/com/example/sweat4success/account/Dashboard.kt
+++ app/src/main/java/com/example/sweat4success/account/Dashboard.kt
@@ -7,6 +7,6 @@ import com.example.sweat4success.R
 class Dashboard: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dashboard)
+        setContentView(R.layout.workout_categories)
     }
 }
